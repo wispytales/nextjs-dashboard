ff57c1dfe5092adfc033e4f0cc7c4d42fb3577b0
onusai <wispytales@gmail.com>
2024-01-24 10:39:51 -0600

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Monoton } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400','700']});
+export const monoton = Monoton({subsets: ['latin'], weight: ['400']});
